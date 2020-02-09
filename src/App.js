1f4d810d123ab7e4341ad191473254ac98c62c69
Federico <fariganello@gmail.com>
2020-02-09 12:01:06 -0300

    boilerplate

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Characters from "./views/Characters";
+import SearchBar from "./views/SearchBar";
+
+
+function App() {
+	return (
+		<div>
+			<SearchBar />
+			<Characters />
+		</div>
+	);
+}
+
+export default App;
