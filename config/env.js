5cd355740457d25c313ddad6a4b55ac26daa9563
Federico <fariganello@gmail.com>
2020-02-12 17:22:51 -0300

    env.js fix2

--- config/env.js
+++ config/env.js
@@ -84,7 +84,7 @@ function getClientEnvironment(publicUrl) {
   
     const env = dotenv.config().parsed;
 
-    const newKeys = (Object.keys(env)||[]).reduce((prev, next) => {
+    const newKeys = Object.keys(env||[]).reduce((prev, next) => {
       prev[next] = env[next];
       return prev;
     }, {});
