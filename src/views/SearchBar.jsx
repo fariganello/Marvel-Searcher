c3cbd8dfb2067b86a02ebde3b9e9960aec5dd74a
Federico <fariganello@gmail.com>
2020-02-12 14:59:00 -0300

    added api folder and fetchCharacters function

--- src/views/SearchBar.jsx
+++ src/views/SearchBar.jsx
@@ -1,18 +1,13 @@
-import axios from "axios";
 import crypto from "crypto-js";
-import React, { useContext, useState } from "react";
-import {Link, useHistory} from "react-router-dom"
+import React, { useState } from "react";
+import { Link } from "react-router-dom"
 
-import MyContext from '../store/store';
+import fetchCharacters from "../api"
 import {MarvelLogo, SearchBarContainer, SearchInput, StyledForm} from "../styles/SearchBarStyles"
 
 export default function SearchBar() {
 	const [searchInput, setSearchInput] = useState("");
-	
-	const [state, dispatch ] = useContext(MyContext)
-	
-	let history = useHistory();
-
+		
 	const PUBLIC_APIKEY = "f1e0fad14a51aa5012fe77652993a1b5"
 	const PRIVATE_APIKEY = "5880a69ada84766a9985e06d0b6315cf6a9f7171" 
 	const timeStamp = "1";
@@ -20,61 +15,16 @@ export default function SearchBar() {
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		axios({
-			method:"get",
-			url:`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${event.target.value}&ts=${timeStamp}&apikey=${PUBLIC_APIKEY}&hash=${hash}`,
-			baseURL: "https://radiant-eyrie-53028.herokuapp.com/"
-		})
-		.then((res) => {
-			return res.data;
-		})
-		.then((res) => {
-			const characters = res.data.results
-			dispatch({
-				type: 'SET_CHARACTERS',
-				characters
-			});
-
-			history.push("/")
-		})
-		.catch((err) => {
-			const characters = []
-			dispatch({
-				type: 'SET_CHARACTERS',
-				characters
-			});	
-			return err;
-		});
+		const url = `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${event.target.value}&ts=${timeStamp}&apikey=${PUBLIC_APIKEY}&hash=${hash}`;
+		const baseUrl = "https://radiant-eyrie-53028.herokuapp.com/";
+		fetchCharacters(url, baseUrl);
 	}
 
 	const handleChange = (event) => {
 		setSearchInput(event.target.value)
-
-		axios({
-			method:"get",
-			 url: `https://gateway.marvel.com/v1/public/characters?nameStartsWith	=${event.target.value}&ts=${timeStamp}&apikey=${PUBLIC_APIKEY}&hash=${hash}`,
-			 baseURL: "https://radiant-eyrie-53028.herokuapp.com/"
-		})
-		.then((res) => {
-			return res.data;
-		})
-		.then((res) => {
-			const characters = res.data.results
-			dispatch({
-				type: 'SET_CHARACTERS',
-				characters
-			});
-
-			history.push("/")
-		})
-		.catch((err) => {
-			const characters = []
-			dispatch({
-				type: 'SET_CHARACTERS',
-				characters
-			});	
-			return err;
-		});
+		const url = `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${event.target.value}&ts=${timeStamp}&apikey=${PUBLIC_APIKEY}&hash=${hash}`;
+		const baseUrl = "https://radiant-eyrie-53028.herokuapp.com/";
+		fetchCharacters(url, baseUrl);
 	}
 
 	return (
